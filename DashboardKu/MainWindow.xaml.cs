4a7888329140b93c24890c440fc5e3caeb1ad5fd
Vito Febrian Ananta <115033527+vitoananta@users.noreply.github.com>
2023-09-21 14:22:04 +0700

    Init

---
+++ DashboardKu/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Windows;
+using System.Windows.Input;
+
+
+namespace DashboardKu
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+    }
+}
